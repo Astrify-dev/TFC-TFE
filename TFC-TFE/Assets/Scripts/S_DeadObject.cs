f33dc86aef1b63e44bfc2974076c8c3c33cc534c
Astrify <145439548+Astrify-dev@users.noreply.github.com>
2025-05-20 22:53:51 +0200

    ennemy qui porusuit le joueur + un material

--- TFC-TFE/Assets/Scripts/S_DeadObject.cs
+++ TFC-TFE/Assets/Scripts/S_DeadObject.cs
@@ -1,38 +1,37 @@
 using UnityEngine;
 
 public class S_DeadObject : MonoBehaviour{
-    //[Header("Paramètres de mort")]
-    //[SerializeField] private bool isGameOverOnDeath = false;
+    [Header("Paramètres de mort")]
+    [SerializeField] private bool isGameOverOnDeath = false;
 
-    //private void OnCollisionEnter(Collision collision){
-    //    if (collision.collider.CompareTag("Player")){
-    //        S_playerStates playerStates = collision.collider.GetComponent<S_playerStates>();
-    //        if (playerStates is not null){
-    //            if (isGameOverOnDeath){
-    //                Debug.Log("Game Over !");
-    //                playerStates.SwitchState(playerStates.DeadState);
-    //            }else{
-    //                RespawnPlayer();
-    //            }
-    //        }
-    //    }
-    //}
+    private void OnCollisionEnter(Collision collision){
+        if (collision.collider.CompareTag("Player")){
+            Debug.Log("Game Over !");
+            /*S_playerStates playerStates = collision.collider.GetComponent<S_playerStates>();
+            if (playerStates is not null){
+                if (isGameOverOnDeath){
+                    //playerStates.SwitchState(playerStates.DeadState);
+                }else{
+                    RespawnPlayer();
+                }
+            }*/
+        }
+    }
 
-    //private void RespawnPlayer(){
-    //    Transform respawnPoint = S_CheckpointManager.instance.GetPlayerRespawnPoint();
-    //    if (respawnPoint is not null){
-    //        GameObject player = GameObject.FindWithTag("Player");
-    //        if (player is not null){
-    //            player.transform.position = respawnPoint.position;
-    //        }
-
-    //        S_DeathFollow deathFollow = FindObjectOfType<S_DeathFollow>();
-    //        if (deathFollow is not null){
-    //            Transform deathRespawnPoint = S_CheckpointManager.instance.GetDeathRespawnPoint();
-    //            if (deathRespawnPoint is not null){
-    //                deathFollow.transform.position = deathRespawnPoint.position;
-    //            }
-    //        }
-    //    }
-    //}
+    private void RespawnPlayer(){
+        Transform respawnPoint = S_CheckpointManager.instance.GetPlayerRespawnPoint();
+        if (respawnPoint is not null){
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player is not null){
+                player.transform.position = respawnPoint.position;
+            }
+            S_DeathFollow deathFollow = FindObjectOfType<S_DeathFollow>();
+            if (deathFollow is not null){
+                Transform deathRespawnPoint = S_CheckpointManager.instance.GetDeathRespawnPoint();
+                if (deathRespawnPoint is not null){
+                    deathFollow.transform.position = deathRespawnPoint.position;
+                }
+            }
+        }
+    }
 }
