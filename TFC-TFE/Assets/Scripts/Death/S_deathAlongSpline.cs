a4d908c49b4a09ac668878fe3078fb4e8ebdfeb4
HuglyDuck <150171589+HuglyDuck@users.noreply.github.com>
2025-05-23 16:22:57 +0200

    ZoneFollow

---
+++ TFC-TFE/Assets/Scripts/Death/S_deathAlongSpline.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class S_deathAlongueSpline : MonoBehaviour
+{
+
+    [SerializeField] SplineContainer _splineFollow;
+    [SerializeField] GameObject _deathObject;
+
+    [SerializeField] float _maxSpeed;
+    [SerializeField] AnimationCurve _speedCurve;
+
+    [SerializeField] Material _materialCorruption;
+
+    [Header("GizmosHelps")]
+    [SerializeField] bool _disableGiemos;
+    [SerializeField] private int _gizmosCount = 100;
+    [SerializeField] private float _gizmosSize = 1;
+
+    public static Action RestDeadZone; 
+
+    private float _splineDistance;
+    private float _currentDistancePercentage;
+
+    private void Awake()
+    {
+        Vector3 NewPosition = _splineFollow.EvaluatePosition(0);
+        _deathObject.transform.position = NewPosition;
+
+    }
+
+    private void Start()
+    {
+        _materialCorruption.SetInt("_EnableEffect", 1);
+        _splineDistance = _splineFollow.CalculateLength();
+        resetDeadZone();
+    }
+
+    private void OnEnable()
+    {
+        RestDeadZone += resetDeadZone;
+    }
+
+    private void OnDisable()
+    {
+        RestDeadZone -= resetDeadZone;
+        _materialCorruption.SetInt("_EnableEffect", 0);
+    }
+
+    private void Update()
+    {
+        float currentSpeed = _speedCurve.Evaluate(_currentDistancePercentage) * _maxSpeed;
+        _currentDistancePercentage += (currentSpeed * Time.deltaTime) / _splineDistance;
+
+        Debug.Log(_splineDistance);
+
+        if (_currentDistancePercentage > 1)
+            EndGame();
+
+
+
+        Vector3 NewPosition = _splineFollow.EvaluatePosition(_currentDistancePercentage);
+
+        _materialCorruption.SetVector("_Coord", NewPosition);
+
+        _deathObject.transform.position = NewPosition;
+    }
+
+    private void EndGame()
+    {
+        S_playerManagerStates ManagerStates = S_controllerPlayer.Instance.PlayerManagerStates;
+
+        ManagerStates.SwitchState(ManagerStates.DeadState);
+
+        resetDeadZone();
+        S_TimerSpeedrun.OnPlayerDeath?.Invoke();
+        S_CanvasEnd.OnPlayerDeath?.Invoke();
+    }
+
+    private void resetDeadZone()
+    {
+        _currentDistancePercentage = 0;
+        Vector3 NewPosition = _splineFollow.EvaluatePosition(0);
+        Debug.Log(NewPosition);
+        _deathObject.transform.position = NewPosition;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(_disableGiemos || _splineFollow is null) return;
+
+        for (int i = 0; i < _gizmosCount; i++)
+        {
+            float Value = (float)i/_gizmosCount;
+            Vector3 GizmosPos = _splineFollow.EvaluatePosition(Value);
+
+            Gizmos.color = Color.Lerp(Color.green, Color.red, _speedCurve.Evaluate(Value));
+            Gizmos.DrawWireSphere(GizmosPos, _gizmosSize);
+
+        }
+
+        
+            
+    }
+
+}
